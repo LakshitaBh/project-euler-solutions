5c5140183f872d1dd6cf49b4c1fdbe8e0fcca228
lakshitabh <lakshitabhargava96@gmail.com>
2025-10-13 21:07:19 +0530

    EP #1 Efficient Solution

--- p001.cpp
+++ p001.cpp
@@ -1,16 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 const int limit = 1000;
 
-int solve(){
-    int sum = 0;
-    for(int i=1;i<limit;i++){
-        sum += (i%3==0||i%5==0)?i:0;
-    }
-    return sum;
+int solve(int div){
+    int lastMultiple = (limit-1)/div;
+    return div*(lastMultiple)*(lastMultiple+1)/2;
 }
 
 int main(){
-    cout<<solve()<<endl;
+    int sum = solve(3) + solve(5) - solve(15);
+    cout<<sum;
 }
